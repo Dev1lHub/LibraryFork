c3b580f215640d3e7ce015006dce123e6eb67dc3
D3v1lHub <gruschkefabian@gmail.com>
2026-08-20 09:43:07 +0200

    Update color scheme in Library.lua

--- Library.lua
+++ Library.lua
@@ -72,11 +72,11 @@ local Library = {
 
     IsLightTheme = false,
     Scheme = {
-        BackgroundColor = Color3.fromRGB(15, 15, 15),
-        MainColor = Color3.fromRGB(25, 25, 25),
-        AccentColor = Color3.fromRGB(125, 85, 255),
-        OutlineColor = Color3.fromRGB(40, 40, 40),
-        FontColor = Color3.new(1, 1, 1),
+        BackgroundColor = Color3.fromRGB(10, 10, 10),
+        MainColor = Color3.fromRGB(18, 18, 18),
+        AccentColor = Color3.fromRGB(255, 255, 255),
+        OutlineColor = Color3.fromRGB(35, 35, 35),
+        FontColor = Color3.new(245, 245, 245),
         Font = Font.fromEnum(Enum.Font.Code),
 
         Red = Color3.fromRGB(255, 50, 50),
